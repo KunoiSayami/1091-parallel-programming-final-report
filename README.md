bf23efd7bb96ae09a30f3abeaed3da51bf4d0883
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2020-12-25 02:24:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# 1091 parallel programming final report
+
+## Result
+
+![](https://user-images.githubusercontent.com/46131041/102508886-dcf6ea80-40c0-11eb-8f9d-072a792234e6.png)
+
+## Special Thanks
+
+Special thanks to [@ayalhw](https://github.com/ayalhw), who helped me configure the cmake.
+
+## License
+
+[![](https://www.gnu.org/graphics/agplv3-155x51.png)](https://www.gnu.org/licenses/agpl-3.0.txt)
+
+Copyright (C) 2020 KunoiSayami
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
---
+++ generate_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 KunoiSayami
+#
+# This module is part of 1091-parallel-programming-final-report and is released under
+# the AGPL v3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+import os
+import random
+
+if __name__ == '__main__':
+    n = random.randint(50, 100)
+    with open('data.in', 'w') as fout:
+        fout.write(f'{n + 3}\n')
+        fout.write(f'{2**23209 - 1}\n')
+        for _i in range(n):
+            sz = [random.randint(0, 9) for _ in range(random.randint(6000, 10000))]
+            sz.append(random.choice(range(1, 10, 2)))
+            print(''.join(map(str, sz)), file=fout)
+        fout.write(f'{2**21701 - 1}\n')
+        fout.write(f'{2**19937 - 1}\n')
+
